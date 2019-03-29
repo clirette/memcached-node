a220b84dfc3b706bacd9405b1b393425e87957b6
Chase Lirette <chaselirette@yahoo.com>
2019-03-29 08:39:47 -0500

    fixing

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Memcached = require('memcached');
-const memcached = new Memcached('localhost:11211', { retry: 5000, idle: 60000, failOverServers: ['localhost:11212'] });
+// const memcached = new Memcached(['10.229.194.249:11211', '10.229.195.108:11211', '10.229.192.134:11211'], { idle: 10000 });
+const memcached = new Memcached('localhost:11211', { idle: 10000 });
 
-memcached.set('foo', 'bar', 20, err => {
-  memcached.get('foo', (err, data) => console.log(data));
+memcached.set('test', 'foo', 20, err => {
+  if (err) console.log(err);
+  memcached.get('test', (err, data) => {
+    if (err) console.log(err);
+    else console.log(data);
+  });
 });
 
 memcached.on('failure', details => {
